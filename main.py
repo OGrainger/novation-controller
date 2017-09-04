24263919162c1bc16634a9469775e08efa59e9af
Oscar Grainger <oscarvgrainger@gmail.com>
2017-09-04 10:04:29 +0200

    2 lights for jenkins status (core + web adm)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import json
 
 compile_core_url = "https://jenkins.ehtrace.com/job/Core/job/Pipelined%20Core-Services/api/json"
 
-# compile_admin_url = "https://jenkins.ehtrace.com/job/Web/job/Admin%20Web/rssAll"
+compile_admin_url = "https://jenkins.ehtrace.com/job/Web/job/Admin%20Web/rssAll"
 
 try:
     import launchpad_py as launchpad
@@ -47,22 +47,38 @@ def main():
     while 1:
         # deploy_core_feed = feedparser.parse(deploy_core_url)
         # deploy_admin_feed = feedparser.parse(deploy_admin_url)
-        # compile_core_data = requests.get(compile_core_url)
+
+        compile_core_data = json.loads(requests.get(compile_core_url).content)
+        compile_web_data = json.loads(requests.get(compile_admin_url).content)
 
         # compile_admin_feed = feedparser.parse(deploy_admin_url)
 
         # print("DEPLOY CORE : " + deploy_core_feed["entries"][0]["title"])
         # print("DEPLOY ADMIN WEB : " + deploy_admin_feed["entries"][0]["title"] + "\n")
         # print(json.loads(compile_core_data.content)["displayName"])
-        # compile_core_isValid = json.loads(compile_core_data.content)["lastCompletedBuild"]["number"] == json.loads(compile_core_data.content)["lastSuccessfulBuild"]["number"]
+
+        if compile_core_data["lastCompletedBuild"]["number"] == compile_core_data["lastSuccessfulBuild"]["number"]:
+            lp.LedCtrlRaw(0, 0, 3)
+        elif compile_core_data["lastCompletedBuild"]["number"] == compile_core_data["lastUnstableBuild"]["number"]:
+            lp.LedCtrlRaw(0, 3, 3)
+        else:
+            lp.LedCtrlRaw(0, 0, 0)
+            time.wait(500)
+            lp.LedCtrlRaw(0, 3, 0)
+
+        if compile_web_data["lastCompletedBuild"]["number"] == compile_web_data["lastSuccessfulBuild"]["number"]:
+            lp.LedCtrlRaw(1, 0, 3)
+        elif compile_web_data["lastCompletedBuild"]["number"] == compile_web_data["lastUnstableBuild"]["number"]:
+            lp.LedCtrlRaw(1, 3, 3)
+        else:
+            lp.LedCtrlRaw(1, 0, 0)
+            time.wait(500)
+            lp.LedCtrlRaw(1, 3, 0)
+
         # print(json.loads(compile_core_data.content)["lastCompletedBuild"]["number"] == json.loads(compile_core_data.content)["lastUnstableBuild"]["number"])
         # print(json.loads(compile_core_data.content)["lastCompletedBuild"]["number"] == json.loads(compile_core_data.content)["lastFailedBuild"]["number"])
         # print(json.loads(compile_core_data.content)["lastFailedBuild"]["number"])
         # print("COMPILE ADMIN WEB : " + compile_admin_feed["entries"][0]["title"] + "\n")
-
-        for i in range(0, 300):
-            lp.LedCtrlRaw(i, 3, 0)
-            time.wait(10)
 
         '''btn = lp.ButtonStateRaw()
 
